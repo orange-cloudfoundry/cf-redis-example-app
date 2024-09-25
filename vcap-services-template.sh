691365852fa625dac21d304ca40f89c129cc7bcf
Olivier ORAND <olivier.orand@orange.com>
2024-09-25 16:26:28 +0000

    chore(github-action): build cnb image - remove host network, use link instead

--- vcap-services-template.sh
+++ vcap-services-template.sh
@@ -1,29 +1,30 @@
 VCAP_SERVICES='
 {
-  "mongodb-dedicated":[
+  "redis-dedicated":[
     {
-      "binding_guid":"${SERVICE_USERNAME}",
+      "binding_guid":"532023be-5847-4dc7-90f4-2f207f2bb440",
       "binding_name":null,
       "credentials":{
-        "database":"${DATABASE_NAME}",
-        "hostname":"localhost:${SERVICE_PORT}",
+        "host":"${SERVICE_HOST}",
         "password":"${SERVICE_PASSWORD}",
-        "uri":"mongodb://${SERVICE_USERNAME}:${SERVICE_PASSWORD}@localhost:${SERVICE_PORT}/${DATABASE_NAME}?replicaSet=rs0",
-        "username":"${SERVICE_USERNAME}"
+        "port":"${SERVICE_PORT}"
       },
-      "instance_guid":"cc252ef5-2541-49fd-8a57-fe73957905a5",
-      "instance_name":"mongodb-github-test",
-      "label":"mongodb-dedicated",
-      "name":"mongodb-github-test",
+      "instance_guid":"98645cee-2810-408c-abcd-2c2b41fed9aa",
+      "instance_name":"redis-smoketest-1727200788",
+      "label":"redis-dedicated",
+      "name":"redis-smoketest-1727200788",
       "plan":"small",
       "provider":null,
       "syslog_drain_url":null,
       "tags":[
-        "mongodb",
-        "document"
+        "Redis",
+        "Document"
       ],
       "volume_mounts":[]
     }
   ]
+
 }
+
+
 '
